50ce34c79b595edc0c8c175611c11934ba0fc44b
jcampeau25 <jdccanuck25@gmail.com>
2024-09-18 08:23:53 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Topic_4___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+
+            Console.WriteLine("Hello! What is your name?" );
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + "! How old are you?");
+            age = Console.
+
+        }
+    }
+}
