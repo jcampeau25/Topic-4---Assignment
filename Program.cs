78418052b4061f436a2a7fb8e2e563ba17d59e37
jcampeau25 <jdccanuck25@gmail.com>
2024-09-18 09:33:46 -0400

    in progress

--- Program.cs
+++ Program.cs
@@ -4,13 +4,24 @@
     {
         static void Main(string[] args)
         {
-            string name;
-            int age;
+            string name, firstName, lastName, login;
+            int age, studentNumber;
+            double pay, average;
 
+
+            // Name + Salary
             Console.WriteLine("Hello! What is your name?" );
             name = Console.ReadLine();
             Console.WriteLine("Hello " + name + "! How old are you?");
-            age = Console.
+            Int32.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine("You're " + age + "? How much do you make a year?");
+            Double.TryParse(Console.ReadLine(), out pay);
+            Console.WriteLine("Wow, you're doing pretty well for yourself");
+            Console.WriteLine();
+
+            // Login
+
+
 
         }
     }
